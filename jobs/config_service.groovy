76d18a64d21e676ce50c51889f6738a8c2555787
Ian Collington <iancollington@gmail.com>
2020-12-31 22:41:23 +0000

    Add website

--- jobs/config_service.groovy
+++ jobs/config_service.groovy
@@ -1,9 +1,5 @@
 multibranchPipelineJob('config-service') {
     branchSources {
-        // git {
-        //     id('bootifulmicropizza-config-service')
-        //     remote('https://github.com/bootifulmicropizza/config-service')
-        // }
         github {
             id('config-service')
             repoOwner('bootifulmicropizza')
@@ -12,7 +8,7 @@ multibranchPipelineJob('config-service') {
     }
     orphanedItemStrategy {
         discardOldItems {
-            numToKeep(20)
+            numToKeep(0)
         }
     }
 }
